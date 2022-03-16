a530f40864318a90ae2c3828d98194fbff31a76f
argzdev <alvinrustan@google.com>
2022-03-17 02:57:19 +0800

    Initial commit

---
+++ app/src/main/java/com/argz/issue3540/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.argz.issue3540
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
